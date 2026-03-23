d0d20a794d66c670ad70d04da0d6b5b85d1a2432
NikhilSai5 <nikhilsaimanam5@gmail.com>
2026-03-23 18:07:22 +0530

    fix next.config for vercel deployment

--- auvia_clinic_side/next.config.ts
+++ auvia_clinic_side/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://10.0.11.76:4002/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
       },
     ];
   },
